13a8a4378d8f58e826da4085fdb621fc799197f7
Eddie Z <e.zarate7@hotmail.com>
2022-09-12 15:14:38 -0600

    Add project files.

---
+++ ApisAndJSON/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+
+
+
+var client = new HttpClient();
+
+
+for (int i = 0; i < 5; i++)
+{
+
+    
+    var kanyeURL = "https://api.kanye.rest/";
+    var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+    var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+    Console.WriteLine($"Kanye: {kanyeQuote}");
+        
+
+    
+    var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+    var ronResponse = client.GetStringAsync(ronURL).Result;
+    var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+    Console.WriteLine($"Ron: {ronQuote}");
+}
